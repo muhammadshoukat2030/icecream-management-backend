d95c2461182eee86aa8033ecb230c8e97840a11a
Muhammad Shoukat <muhammadshoukat2030@gmail.com>
2026-09-14 04:21:13 -0700

    api and route add successfully updated

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 const allowedOrigins = [
     "http://127.0.0.1:5501",
     "http://localhost:5501",
-    "https://icecream-management-opal.vercel.app",
+    "https://icecream-management-frontend.vercel.app",
 ];
 
 app.use(cors({
